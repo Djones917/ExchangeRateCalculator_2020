6038a292551c4c8561ac5026b45f17d714e0a1dd
Derek <nightwing917@hotmail.com>
2020-06-07 21:10:19 -0600

    Finished

--- app.js
+++ app.js
@@ -14,8 +14,15 @@ function calculate() {
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
 
-    fetch(`https://v6.exchangerate-api.com/v6/112d402dd268db686bfa0484/latest/${currency_one}`).then(res => res.json()).then(data => {
-        console.log(data);
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
 }
 
@@ -28,6 +35,13 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+});
 
 
 
